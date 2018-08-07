9b6ccfd08b0669a7d665aa43ec75d5ba4fb73ef9
kasulejoseph <kasule06joseph@gmail.com>
2018-08-07 11:23:08 +0300

    commit

---
+++ data.py
@@ -0,0 +1,23 @@
+def Articles():
+    articles = [
+        {
+           'id':1,
+           'title':'Article one',
+           'body':'hgihuitghgu ugfihfgiu',
+           'create_date':'04-05-2018' 
+        },
+        {
+           'id':2,
+           'title':'Article Two',
+           'body':'hgihuitghgu ugfihfgiu',
+           'create_date':'04-05-2018' 
+        },
+        {
+           'id':3,
+           'title':'Article Three',
+           'body':'hgihuitghgu ugfihfgiu',
+           'create_date':'04-05-2018' 
+        }
+
+    ]
+    return articles
